38ef52b88f1f3905a3c719b4e2f4b610021c1f34
Reza Aditya <reza.aditya@pelni.co.id>
2025-03-23 09:58:02 +0700

    update constants

--- data/constants.ts
+++ data/constants.ts
@@ -2,11 +2,11 @@
  * LINKS and USERNAMES
  */
 export const GITHUB_API_URL = 'https://api.github.com';
-export const GITHUB_USERNAME = 'MA-Ahmad';
-export const GITHUB_REPO = 'myportfolio';
+export const GITHUB_USERNAME = 'zayano';
+export const GITHUB_REPO = 'myPortfolioReza';
 export const DEVTO_API_URL = 'https://dev.to/api';
 
 /**
  * SEO constants
  */
-export const SEO_IMAGE = 'https://mahmad.me/logo.png';
+export const SEO_IMAGE = 'https://avatars.githubusercontent.com/u/24294307?v=4';
