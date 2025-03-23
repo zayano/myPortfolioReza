9900b6e76f07c767ba6f4dd26f7ab8a013e5c7d5
Reza Aditya <reza.aditya@pelni.co.id>
2025-03-23 10:33:14 +0700

    update username

--- lib/fetchPosts.ts
+++ lib/fetchPosts.ts
@@ -32,7 +32,7 @@ export const getDbPosts = () => {
 };
 
 export const getDevtoPosts = async () => {
-  const res = await fetch(`${DEVTO_API_URL}/articles?username=${process.env.DEVTO_USERNAME}`);
+  const res = await fetch(`${DEVTO_API_URL}/articles?username=zayano`);
 
   if (res.status < 200 || res.status >= 300) {
     throw new Error(`Error fetching... Status code: ${res.status}, ${res.statusText}`);
