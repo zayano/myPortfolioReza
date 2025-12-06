4f49742e11bc854e54d45abd39ad61c85b2b2d5d
Reza Aditya Sutrisno <adityareza46@gmail.com>
2025-12-06 09:14:53 +0700

    update site contacts

--- configs/site-config.js
+++ configs/site-config.js
@@ -1,44 +1,45 @@
-import {
-  FaGithub,
-  FaDev,
-  FaLinkedin,
-  FaTwitter
-} from "react-icons/fa";
-import { FiMail } from "react-icons/fi";
+import { FaGithub, FaDev, FaLinkedin, FaTwitter, FaInstagram } from 'react-icons/fa';
+import { FiMail } from 'react-icons/fi';
 
 const siteConfig = {
   copyright: `Copyright © ${new Date().getFullYear()} Reza Aditya Sutrisno. All Rights Reserved.`,
   author: {
-    name: "Reza Aditya Sutrisno",
+    name: 'Reza Aditya Sutrisno',
     accounts: [
       {
-        url: "https://github.com/zayano",
-        label: "Github Account",
-        type: "gray",
+        url: 'https://github.com/zayano',
+        label: 'Github Account',
+        type: 'gray',
         icon: <FaGithub />
       },
       {
-        url: "https://x.com/Zaditno",
-        label: "Twitter Account",
-        type: "twitter",
+        url: 'https://www.instagram.com/zaditno',
+        label: 'Instagram Account',
+        type: 'gray',
+        icon: <FaInstagram />
+      },
+      {
+        url: 'https://x.com/Zaditno',
+        label: 'Twitter Account',
+        type: 'twitter',
         icon: <FaTwitter />
       },
       {
-        url: "https://dev.to/zayano",
-        label: "Dev Account",
-        type: "gray",
+        url: 'https://dev.to/zayano',
+        label: 'Dev Account',
+        type: 'gray',
         icon: <FaDev />
       },
       {
-        url: "https://www.linkedin.com/in/reza-aditya-sutrisno-9b8519161/",
-        label: "LinkedIn Account",
-        type: "linkedin",
+        url: 'https://www.linkedin.com/in/reza-aditya-sutrisno-9b8519161/',
+        label: 'LinkedIn Account',
+        type: 'linkedin',
         icon: <FaLinkedin />
       },
       {
-        url: "mailto:adityareza46@@gmail.com",
-        label: "Mail Reza",
-        type: "gray",
+        url: 'mailto:adityareza46@gmail.com',
+        label: 'Mail Reza',
+        type: 'gray',
         icon: <FiMail />
       }
     ]
