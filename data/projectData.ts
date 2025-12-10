24ab9d5cda962ed478825bc9a461cad2b4b05418
Reza Aditya Sutrisno <adityareza46@gmail.com>
2025-12-10 20:28:54 +0700

    update list project

--- data/projectData.ts
+++ data/projectData.ts
@@ -9,24 +9,6 @@ export const projectsList = [
     techStack: ['Nextjs', 'Tailwind', 'Typescript']
   },
   {
-    title: 'Depok Single Window',
-    imageLight: '/assets/images/projects/cover/dsw.png',
-    blurHash: 'L4ADc400P*Zi4Tu1y;Qo00pH#YXl',
-    site: 'https://dsw.depok.go.id/',
-    description:
-      'City services and information center in one application and can provide easy access to the public in finding information about the city government.',
-    techStack: ['Kotlin', 'JSON', 'Android']
-  },
-  {
-    title: 'Digital Signage',
-    imageLight: '/assets/images/projects/cover/ds.png',
-    blurHash: 'L4ADc400P*Zi4Tu1y;Qo00pH#YXl',
-    site: 'https://www.sentuh.id/digital-signage',
-    description:
-      ' A visual communication solution that uses electronic screens to display information, advertisements, videos, images, or dynamic multimedia content.',
-    techStack: ['Java', 'JSON', 'Android']
-  },
-  {
     title: 'Wifi.Id Go Mobile',
     imageLight: '/assets/images/projects/cover/wigo.png',
     blurHash: 'L4ADc400P*Zi4Tu1y;Qo00pH#YXl',
@@ -42,39 +24,23 @@ export const projectsList = [
     description:
       'The TransJakarta & JakLingko VMS application is an application owned by Trans Jakarta and JakLingko which is used to assist users in making agreements for business, training, visits, meetings, and others by using the face recognition feature or face detection. And here is an overview of the application.',
     techStack: ['Flutter', 'Dart', 'MLKit']
+  },
+  {
+    title: 'Digital Signage',
+    imageLight: '/assets/images/projects/cover/ds.png',
+    blurHash: 'L4ADc400P*Zi4Tu1y;Qo00pH#YXl',
+    site: 'https://www.sentuh.id/digital-signage',
+    description:
+      ' A visual communication solution that uses electronic screens to display information, advertisements, videos, images, or dynamic multimedia content.',
+    techStack: ['Java', 'JSON', 'Android']
+  },
+  {
+    title: 'Depok Single Window',
+    imageLight: '/assets/images/projects/cover/dsw.png',
+    blurHash: 'L4ADc400P*Zi4Tu1y;Qo00pH#YXl',
+    site: 'https://dsw.depok.go.id/',
+    description:
+      'City services and information center in one application and can provide easy access to the public in finding information about the city government.',
+    techStack: ['Kotlin', 'JSON', 'Android']
   }
-  // {
-  //   title: 'Workof Ecommerce',
-  //   imageLight: '/assets/images/projects/cover/workof.png',
-  //   blurHash: 'L2M5%%0000EN00I:-oIp00kC?^sl',
-  //   site: 'https://workof.com/',
-  //   description:
-  //     'This is an Ecommerce furniture site. It provides a single platform to multiple vendors where they can put their products on sale.',
-  //   techStack: ['Rails', 'Javascript']
-  // },
-  // {
-  //   title: 'Souq of Qatar',
-  //   imageLight: '/assets/images/projects/cover/souq.png',
-  //   blurHash: 'L1O^uE0679I00AGT?.H{00y7p2tK',
-  //   site: 'https://www.souqofqatar.com/',
-  //   description:
-  //     'Souq of Qatar is online comparison website. This website is independent of any retailers, it displays the products of all the major stores.',
-  //   techStack: ['Rails', 'Javascript']
-  // },
-  // {
-  //   title: 'GoSave',
-  //   imageLight: '/assets/images/projects/cover/gosave.png',
-  //   blurHash: 'L09sWSxdH;s?%6axWAfk00V?%jax',
-  //   site: 'https://gosavehq.com/',
-  //   description: `The GoSave app let's you send allowances, assign chores, and approve spending all from your smartphone.`,
-  //   techStack: ['Rails']
-  // },
-  // {
-  //   title: 'Hiredroid',
-  //   imageLight: '/assets/images/projects/cover/hiredroid.png',
-  //   blurHash: 'L09sWSxdH;s?%6axWAfk00V?%jax',
-  //   site: 'https://hiredroid.com',
-  //   description: `Hiredroid help professionals reach the very pinnacle and stay there. For professionals who aspire perfection, we have made it easy for you by introducing skill-based hiring system, to help you attract the best talent from around the world.`,
-  //   techStack: ['React', 'Nodejs']
-  // }
 ];
